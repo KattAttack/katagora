05acf54ccdf0fe01cdeb27f85744805bf725d672
KattAttack <kozorezovk@outlook.com>
2019-06-21 00:28:51 -0700

    Fixed html dom and vertical layout of css

--- js/Router.js
+++ js/Router.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Welcome } from "./Welcome";
 import { About } from "./About";
+import { Portfolio } from "./Portfolio";
 
 export default function Router(props) {
 	switch (props.page) {
 		case "about":
 			return <About />;
+
+		case "portfolio":
+			return <Portfolio />;
 
 		case "welcome":
 			return <Welcome />;
