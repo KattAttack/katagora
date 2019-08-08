6d43a912191fdd56018e1bf092c8cbe4c0703314
KattAttack <kozorezovk@outlook.com>
2019-08-07 22:29:22 -0700

    altered menu bubbles animation

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ import Router from "./Router";
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { page: "", showMenu: false };
+		this.state = { page: "", showGirl: false, showGram: false, showHouse: false };
 		this.loadWelcome = this.loadWelcome.bind(this);
 		this.loadAbout = this.loadAbout.bind(this);
 		this.loadPortfolio = this.loadPortfolio.bind(this);
@@ -106,38 +106,63 @@ class App extends React.Component {
 		});
 
 		//Nav hover & click effects
+
 		const girl = document.getElementById("girl");
 		var girlText = document.getElementById("girlText");
+
 		girl.addEventListener("mouseover", e => {
 			girlText.className = "typewriter";
+			this.setState({
+				showGirl: true
+			});
 		});
 
 		girl.addEventListener("mouseout", e => {
 			girlText.className = "hidden";
+			this.setState({
+				showGirl: false
+			});
 		});
 
+		const gramContainer = document.getElementById("gramContainer");
 		const gram = document.getElementById("gram");
 		var gramText = document.getElementById("gramText");
 		gram.addEventListener("mouseover", e => {
 			gramText.className = "typewriter";
+			this.setState({
+				showGram: true
+			});
 		});
 
 		gram.addEventListener("mouseout", e => {
 			gramText.className = "hidden";
+			this.setState({
+				showGram: false
+			});
 		});
 
+		const houseContainer = document.getElementById("houseContainer");
 		const house = document.getElementById("house");
 		var houseText = document.getElementById("houseText");
 		house.addEventListener("mouseover", e => {
 			houseText.className = "typewriter";
+			this.setState({
+				showHouse: true
+			});
 		});
 
 		house.addEventListener("mouseout", e => {
 			houseText.className = "hidden";
+			this.setState({
+				showHouse: false
+			});
 		});
 	}
 
 	render() {
+		// if (this.state.page === "tictactoe") {
+		// 	return <Router page={this.state.page} />;
+		// }
 		return (
 			<div className='App'>
 				<img className='backgroundImage' src='background/redbackground.jpg' />
@@ -170,28 +195,35 @@ class App extends React.Component {
 				</div>
 
 				<div className='nav'>
-					<ul
-						style={{ listStyle: "none" }}
-						onMouseEnter={() => {
-							this.menuShow();
-						}}
-						onMouseLeave={() => {
-							this.menuHide();
-						}}
-					>
+					<ul style={{ listStyle: "none" }}>
 						<li>
 							<div className='container'>
+								<div className='menuBackground'>
+									<div
+										className={this.state.showGirl ? "girlShow" : "girlHide"}
+									/>
+								</div>
 								<img
 									className='resize'
 									id='girl'
 									src='girl.png'
 									alt='Girl'
 									onClick={this.loadAbout}
+									onMouseEnter={() => {
+										this.setState({
+											showGirl: true
+										});
+									}}
+									onMouseLeave={() => {
+										this.setState({
+											showGirl: false
+										});
+									}}
 								/>
 
 								<div id='girlText' className='hidden'>
 									<h1 style={{ paddingTop: "6.5px", marginLeft: "0px" }}>
-										&nbsp;> Who is She
+										&nbsp;Who is She
 									</h1>
 								</div>
 							</div>
@@ -199,6 +231,12 @@ class App extends React.Component {
 
 						<li>
 							<div className='container'>
+								<div className='menuBackground'>
+									<div
+										className={this.state.showGram ? "gramShow" : "gramHide"}
+									/>
+								</div>
+
 								<img
 									className='navImage'
 									id='gram'
@@ -206,10 +244,21 @@ class App extends React.Component {
 									alt='Instagram'
 									width='50'
 									height='50'
+									// onClick={this.loadTictactoe}
+									// onMouseEnter={() => {
+									// 	this.setState({
+									// 		showGram: true
+									// 	});
+									// }}
+									// onMouseLeave={() => {
+									// 	this.setState({
+									// 		showGram: false
+									// 	});
+									// }}
 								/>
 
 								<div id='gramText' className='hidden'>
-									<h1 style={{ paddingTop: "5px", marginLeft: "36px" }}>
+									<h1 style={{ paddingTop: "5px", marginLeft: "10px" }}>
 										&nbsp;Off the Gram
 									</h1>
 								</div>
@@ -218,6 +267,12 @@ class App extends React.Component {
 
 						<li>
 							<div className='container'>
+								<div className='menuBackground'>
+									<div
+										className={this.state.showHouse ? "houseShow" : "houseHide"}
+									/>
+								</div>
+
 								<img
 									className='navImage'
 									id='house'
@@ -226,10 +281,20 @@ class App extends React.Component {
 									width='50'
 									height='50'
 									onClick={this.loadPortfolio}
+									onMouseEnter={() => {
+										this.setState({
+											showHouse: true
+										});
+									}}
+									onMouseLeave={() => {
+										this.setState({
+											showHouse: false
+										});
+									}}
 								/>
 
 								<div id='houseText' className='hidden'>
-									<h1 style={{ paddingTop: "6px", marginLeft: "36px" }}>
+									<h1 style={{ paddingTop: "6px", marginLeft: "10px" }}>
 										&nbsp;Portfolio
 									</h1>
 								</div>
@@ -238,9 +303,9 @@ class App extends React.Component {
 					</ul>
 				</div>
 
-				<div className={this.state.showMenu ? "sunHide" : "sunShow"} />
+				{/* <div className={this.state.showMenu ? "sunHide" : "sunShow"} /> */}
 
-				<div className={this.state.showMoon ? "moonShow" : "moonHide"} />
+				{/* <div className={this.state.showMoon ? "moonShow" : "moonHide"} /> */}
 
 				<img className='columnRight' src='columns/pinkcolumn2.png' />
 			</div>
