48ea0babf1c88e3977aaa22bf250269ce155dd3f
KattAttack <kozorezovk@outlook.com>
2019-08-11 22:22:33 -0700

    Altered css widths and edited about page dropdowns

--- js/About.js
+++ js/About.js
@@ -75,27 +75,22 @@ export class About extends React.Component {
 							}}
 							className='aboutMeText'
 						>
-							<div
-								className='aboutYellowBox'
-								style={{ maxHeight: this.state.showMe ? 500 : 0 }}
-							/>
 							<div className='A1'>My relationship with identity is complicated</div>
 
 							<div className='A2'>
-								and a tinder or bumblebff or Instagram bio section or LinkedIn about
-								me post just won't cut it. Refined, character restricted, selective,
-								filtered, these entries are good for what they're used for, but what
-								I choose to share here is more intimate than carnal desire and more
-								personal than job placement. I was born in America but I am Russian.
-								Though that's my ethnicity, I don't particularly care for it. Though
-								I'm not Greek I am intrigued. Though I'm 23 there are days I feel 40
-								and days I feel 5. I will tell you my dreams, I will share my doubts
-								and worries. I will open a window and you will see the world as I
-								do. This will be fluid, it'll change it'll grow. I will ramble,
-								quote, spout prose, fuck up. And hopefully by the end we'll have
-								gotten so far that the beginning and our questions of identity go to
-								rest in the folds of our smiles. Some day you'll know more, but for
-								now, this is it.
+								and an Instagram or LinkedIn bio just won't cut it. Filtered,
+								selective, and character restricted, these entries are good for what
+								they're used for. But what I choose to share here goes beyond a
+								quick post and is more intimate than job placement. I was born in
+								America but I am Russian. Though that's my ethnicity, I don't
+								particularly care for it. Though I'm not Greek I am intrigued.
+								Though I'm 23 there are days I feel 40 and days I feel 5. I will
+								tell you my dreams, I will share my doubts and worries. I will open
+								a window and you will see the world as I do. This will be fluid,
+								it'll change it'll grow. I will ramble, quote, spout prose, fuck up.
+								And hopefully by the end we'll have gotten so far that the beginning
+								and our questions of identity go to rest in the folds of our smiles.
+								Some day you'll know more, but for now, this is it.
 							</div>
 						</div>
 					</div>
@@ -123,10 +118,6 @@ export class About extends React.Component {
 							}}
 							className='aboutRelText'
 						>
-							<div
-								className='relYellowBox'
-								style={{ maxHeight: this.state.showRel ? 500 : 0 }}
-							/>
 							<div className='A1'>My relationship with identity is complicated</div>
 							<div className='A2'>
 								and a tinder or bumblebff or Instagram bio or LinkedIn about me post
@@ -170,10 +161,6 @@ export class About extends React.Component {
 							}}
 							className='aboutGoalsText'
 						>
-							<div
-								className='goalsYellowBox'
-								style={{ maxHeight: this.state.showGoals ? 500 : 0 }}
-							/>
 							<div className='A1'>My relationship with identity is complicated</div>
 							<div className='A2'>
 								and a tinder or bumblebff or Instagram bio or LinkedIn about me post
@@ -217,10 +204,6 @@ export class About extends React.Component {
 							}}
 							className='aboutInfoText'
 						>
-							<div
-								className='infoYellowBox'
-								style={{ maxHeight: this.state.showInfo ? 500 : 0 }}
-							/>
 							<div className='A1'>My relationship with identity is complicated</div>
 							<div className='A2'>
 								and a tinder or bumblebff or Instagram bio or LinkedIn about me post
