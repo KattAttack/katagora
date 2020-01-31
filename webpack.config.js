db70019f434622b7c53d9471460448f7ee37ab30
KattAttack <kozorezovk@outlook.com>
2020-01-30 23:10:13 -0800

    Fixed webpack babel plugin errors

--- webpack.config.js
+++ webpack.config.js
@@ -26,9 +26,8 @@ module.exports = env => {
 						loader: "babel-loader",
 						options: {
 							presets: [ "@babel/preset-env", "@babel/preset-react" ],
-							plugins: [ "@babel/plugin-proposal-class-properties" ],
-							plugins: [ "@babel/plugin-syntax-dynamic-import" ],
-							plugins: [ "@babel/plugin-proposal-class-properties" ]
+							plugins: [ "@babel/plugin-proposal-class-properties", "@babel/plugin-syntax-dynamic-import" ]
+
 						}
 					}
 				},
